249d9af5565ea955cd065890a349d67c66427cbf
liuyun <liuyunplus@gmail.com>
2022-01-07 15:17:06 +0800

    fix

---
+++ 8-微信应用消息推送/Main.py
@@ -0,0 +1,31 @@
+import requests
+
+
+def getAccessToken():
+    params = {
+        # 企业ID
+        "corpid": "wwd5e52a75bfc03886",
+        # 企业应用密钥
+        "corpsecret": ""
+    }
+    url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken"
+    res = requests.get(url, params=params).json()
+    return res['access_token']
+
+
+def sendTextMsg(access_token, message):
+    url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=" + access_token
+    data = {
+        "touser": "@all",
+        "msgtype": "text",
+        "agentid": "1000002",
+        "text": {
+            "content": message
+        }
+    }
+    res = requests.post(url, json=data).json()
+    print(res)
+
+
+accessToken = getAccessToken()
+sendTextMsg(accessToken, "今日行情数据")
