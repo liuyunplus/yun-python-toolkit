8f0871b224aa99be2bd61ea786b25d2b4349b4cd
LiuYun <liuyunplus@gmail.com>
2022-06-15 16:45:03 +0800

    fix

--- 9-爬虫/Config.py
+++ 9-爬虫/Config.py
@@ -1,5 +1,5 @@
 
-FILE_NAME = 'data.txt'
+FILE_NAME = 'data'
 SENDER_EMAIL = '1161305061@qq.com'
 SENDER_PASSWORD = 'bwlanuavdmbijjff'
 RECEIVERS = ['731371050@qq.com']
--- 9-爬虫/Spider.py
+++ 9-爬虫/Spider.py
@@ -1,78 +1,56 @@
 import re
-import time
+from email.mime.text import MIMEText
+from smtplib import SMTP_SSL
 import requests
 from bs4 import BeautifulSoup
-import EmailUtils
-from playwright.sync_api import sync_playwright
-
-header = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
-    "Cache-Control": "no-cache",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36",
-}
-
-user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36'
+import Config
 
 
 def get_data_list():
+    list = []
     url = "https://www.hermes.cn/cn/zh/category/%E5%A5%B3%E5%A3%AB/%E7%AE%B1%E5%8C%85%E5%B0%8F%E7%9A%AE%E5%85%B7/%E7" \
           "%AE%B1%E5%8C%85%E6%99%9A%E5%AE%B4%E5%8C%85/#|"
-    list = []
-    with sync_playwright() as p:
-        browser = p.chromium.launch()
-        browser.new_context(java_script_enabled=True, user_agent=user_agent)
-        page = browser.new_page()
-        page.goto(url)
-        page_content = page.content()
-        soup = BeautifulSoup(page_content, 'lxml')
-        tag_list = soup.find_all('div', {"class": re.compile(r".*product-grid-list-item.*")})
-        for tag in tag_list:
-            data = {}
-            meta = tag.find('div', {"class": "product-item-meta"})
-            data['id'] = meta['id']
-            link = tag.find('a', {"href": True})
-            data['detail_page'] = 'https://www.hermes.cn' + link['href']
-            img = tag.find('img')
-            data['image_url'] = 'http:' + img['src']
-            name = tag.find('h4', {"class": "product-item-name"})
-            data['name'] = name.text
-            price = tag.find('div', {"class": re.compile(r".*product-item-price.*")})
-            price_text = price.text.replace(",", "")
-            price_text = price_text.replace(" ", "")
-            data['price'] = price_text
-            list.append(data)
-        browser.close()
+    payload = ""
+    headers = {
+        "cookie": "_gcl_au=1.1.680716924.1654744267; Hm_lvt_e6c348f2c9b16d8b005a379023cce3b3=1654744259,1655113152,1655263855; _gid=GA1.2.249683741.1655263855; _cs_mk=0.15408045765717504_1655278395101; ECOM_SESS=c42mf15mn722tjrsplcqcsl1t0; correlation_id=ea48435e46229a93b8f783a9ba9e0ad16d554e141e7184bd191c1865d359e295; datadome=.CuUrnY30lMAvcjIqmSBrtJK4R1gILdxrLxSk7Dpw2tQ-77sZ4wDZ6t8tIsgblWRMPM5Vpa.ERuoDp.u0Eel_qUee8pMd7XG_skOxXf1jwzI3~KXOBSIy8c6~65Tq-AH; Hm_lpvt_e6c348f2c9b16d8b005a379023cce3b3=1655281434; _ga_Y862HCHCQ7=GS1.1.1655278396.6.1.1655281435.0; _ga=GA1.1.931983995.1654744259",
+        "authority": "www.hermes.cn",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+        "cache-control": "no-cache",
+        "pragma": "no-cache",
+        "sec-ch-device-memory": "8",
+        "sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+        "sec-ch-ua-arch": 'x86',
+        "sec-ch-ua-full-version-list": '" Not A;Brand";v="99.0.0.0", "Chromium";v="102.0.5005.61", "Google Chrome";v="102.0.5005.61"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-model": "",
+        "sec-ch-ua-platform": "macOS",
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
+    }
+    response = requests.request("GET", url, data=payload, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+    tag_list = soup.find_all('div', {"class": re.compile(r".*product-grid-list-item.*")})
+    for tag in tag_list:
+        data = {}
+        meta = tag.find('div', {"class": "product-item-meta"})
+        data['id'] = meta['id']
+        link = tag.find('a', {"href": True})
+        data['detail_page'] = 'https://www.hermes.cn' + link['href']
+        img = tag.find('img')
+        data['image_url'] = 'http:' + img['src']
+        name = tag.find('h4', {"class": "product-item-name"})
+        data['name'] = name.text
+        price = tag.find('div', {"class": re.compile(r".*product-item-price.*")})
+        price_text = price.text.replace(",", "")
+        price_text = price_text.replace(" ", "")
+        data['price'] = price_text
+        list.append(data)
     return list
-
-
-
-def get_data_detail(data_list):
-    header['referer'] = 'https://www.hermes.cn/cn/zh/story/291557-watches-women/'
-    with sync_playwright() as p:
-        browser = p.chromium.launch()
-        browser.new_context(java_script_enabled=True, user_agent=user_agent)
-        for data in data_list:
-            is_next = True
-            while is_next:
-                try:
-                    page = browser.new_page()
-                    page_path = data['detail_page']
-                    page.goto(page_path)
-                    page_content = page.content()
-                    soup = BeautifulSoup(page_content)
-                    button = soup.find('button', {"name": "add-to-cart"})
-                    is_disabled = button.has_attr('disabled')
-                    data['is_disabled'] = is_disabled
-                    is_next = False
-                except:
-                    print("request error")
-                    time.sleep(3)
-            time.sleep(3)
-        browser.close()
-    return data_list
 
 
 def get_new_items(data_list):
@@ -88,20 +66,34 @@ def get_new_items(data_list):
 
 
 def read_lines():
-    with open('data.txt', 'r') as file:
+    with open(Config.FILE_NAME, 'r') as file:
         lines = file.read().splitlines()
     return lines
 
 
 def write_lines(item_list):
-    with open("data.txt", 'w') as file:
+    with open(Config.FILE_NAME, 'w') as file:
         file.write("\n".join(item_list))
+
+
+def send_mail(receivers, new_list):
+    if len(new_list) == 0:
+        return
+    # 填写真实的发邮件服务器用户名、密码
+    user = '1161305061@qq.com'
+    password = 'bwlanuavdmbijjff'
+    # 邮件内容
+    msg = MIMEText("\n".join(new_list), 'plain', _charset="utf-8")
+    # 邮件主题描述
+    msg["Subject"] = "爱马仕商品上新监控"
+    with SMTP_SSL(host="smtp.qq.com", port=465) as smtp:
+        # 登录发邮件服务器
+        smtp.login(user=user, password=password)
+        # 实际发送、接收邮件配置
+        smtp.sendmail(from_addr=user, to_addrs=receivers.split(','), msg=msg.as_string())
 
 
 data_list = get_data_list()
 new_list = get_new_items(data_list)
-dass = get_data_detail(data_list)
-print(dass)
-EmailUtils.send_mail("731371050@qq.com", "爱马仕商品上新监控", "\n".join(new_list))
-
-print(new_list)
+send_mail("liuyunplus@163.com", new_list)
+print('执行成功')
