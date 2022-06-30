fbee0bc465793da0be0cab2131f38764ecfb22ab
LiuYun <liuyunplus@gmail.com>
2022-06-30 15:48:10 +0800

    fix

--- 9-爬虫/Config.py
+++ 9-爬虫/Config.py
@@ -1,5 +1,5 @@
 
-FILE_NAME = 'data'
+FILE_NAME = '/Users/dr.panda/Public/MyPro/yun-python-toolkit/9-爬虫/data'
 SENDER_EMAIL = '1161305061@qq.com'
 SENDER_PASSWORD = 'bwlanuavdmbijjff'
 RECEIVERS = ['731371050@qq.com']
--- 9-爬虫/Spider.py
+++ 9-爬虫/Spider.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
 import re
 from email.mime.text import MIMEText
 from smtplib import SMTP_SSL
